5a8740f213136db5fc376eabd6566f7d6d39565a
Adriano Marques <adriano.mqs@gmail.com>
2018-02-14 16:19:48 -0200

    Adicionando Produto

--- src/main/java/com/flexautomacao/cursomc/CursomcApplication.java
+++ src/main/java/com/flexautomacao/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.flexautomacao.cursomc.domain.Categoria;
+import com.flexautomacao.cursomc.domain.Produto;
 import com.flexautomacao.cursomc.repositories.CategoriaRepository;
+import com.flexautomacao.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
-	private CategoriaRepository repo;
+	private CategoriaRepository categoryRepo;
+	
+	@Autowired
+	private ProdutoRepository produtoRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -26,7 +31,20 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
-		repo.save(Arrays.asList(cat1, cat2));
+		Produto p1 = new Produto(null, "Computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 800.0);
+		Produto p3 = new Produto(null, "Mouse", 70.0);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+				
+		categoryRepo.save(Arrays.asList(cat1, cat2));
+		produtoRepo.save(Arrays.asList(p1, p2, p3));
+		
 		
 	}
 }
---
+++ src/main/java/com/flexautomacao/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.flexautomacao.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.flexautomacao.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+}
