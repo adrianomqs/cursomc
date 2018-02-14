76dd2ad3fbe43c2131dd9d812a15507578b00d05
Adriano Marques <adriano.mqs@gmail.com>
2018-02-14 16:42:45 -0200

    ajustando endpoint de erro objeto nao encontrado

--- src/main/java/com/flexautomacao/cursomc/services/CategoriaService.java
+++ src/main/java/com/flexautomacao/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.flexautomacao.cursomc.domain.Categoria;
 import com.flexautomacao.cursomc.repositories.CategoriaRepository;
+import com.flexautomacao.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,10 +17,21 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: "+ Categoria.class.getName());
+		}
+		
 		return obj;
 	}
 	
 	public List<Categoria> buscarTodos(){
-		return repo.findAll();
+		List<Categoria> obj = repo.findAll();
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Tipo: "+ Categoria.class.getName());
+		}
+		
+		return obj;
 	}
 }
